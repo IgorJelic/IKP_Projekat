48658f526704c2140a159dae7876f3ab044d9ad0
Igor Jelic <igor.jelic.98@gmail.com>
2021-01-12 18:16:31 +0100

    Added some commented code on client side

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -72,7 +72,34 @@ int main()
 
 	while (true)
 	{
-		printf("Added develop branch.");
+		//// Unos potrebnih podataka koji ce se poslati serveru
+		//printf("Unesite ime studenta: ");
+		//gets_s(student.ime, 15);
+
+		//printf("Unesite prezime studenta: ");
+		//gets_s(student.prezime, 20);
+
+		//printf("Unesite osvojene poene na testu: ");
+		//scanf("%d", &poeni);
+		//student.poeni = htons(poeni);  //obavezna funkcija htons() jer cemo slati podatak tipa short 
+		//getchar();    //pokupiti enter karakter iz bafera tastature
+
+
+		//// Slanje pripremljene poruke zapisane unutar strukture studentInfo
+		////prosledjujemo adresu promenljive student u memoriji, jer se na toj adresi nalaze podaci koje saljemo
+		////kao i velicinu te strukture (jer je to duzina poruke u bajtima)
+		//iResult = send(connectSocket, (char*)&student, (int)sizeof(studentInfo), 0);
+
+		//// Check result of send function
+		//if (iResult == SOCKET_ERROR)
+		//{
+		//	printf("send failed with error: %d\n", WSAGetLastError());
+		//	closesocket(connectSocket);
+		//	WSACleanup();
+		//	return 1;
+		//}
+
+		//printf("Message successfully sent. Total bytes: %ld\n", iResult);
 
 		printf("\nPress 'x' to exit or any other key to continue: ");
 		if (_getch() == 'x')
